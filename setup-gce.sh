752f9505148fdcb4a63924e7b56b972379c5bea4
kk1694 <42827987+kk1694@users.noreply.github.com>
2018-11-01 15:00:33 +0700

    Change to pytorch 0.4
    Instead of installing fastai and the pytorch preview, don't install fastai, and install the stable pytorch version.

--- setup-gce.sh
+++ setup-gce.sh
@@ -16,18 +16,14 @@ set -e
 # source ~/.bashrc has no effect here: https://stackoverflow.com/a/43660876/457224
 export PATH="$HOME/anaconda3/bin:$PATH"
 
-conda create -y --name fastai-v1 python=3.7
+conda create -y --name pytorch_env python=3.7
 
-source activate fastai-v1
+source activate pytorch_env
+conda install pytorch torchvision -c pytorch
 
-conda install -y -c pytorch pytorch-nightly cuda92
-conda install -y -c fastai torchvision-nightly
-conda install -y -c fastai fastai
+source activate pytorch_env
+python -m ipykernel install --user --name pytorch_env --display-name "pytorch_env"
 
-source activate fastai-v1
-python -m ipykernel install --user --name fastai-v1 --display-name "fastai-v1"
-
-git clone https://github.com/fastai/course-v3.git
 
 ## Install the start script
 cat > /tmp/jupyter.service <<EOL
@@ -41,7 +37,7 @@ Type=simple
 Restart=always
 RestartSec=1
 User=$USER
-ExecStart=$HOME/anaconda3/bin/jupyter lab --ip 0.0.0.0 --notebook-dir $HOME '--KernelSpecManager.whitelist=["fastai-v1"]'
+ExecStart=$HOME/anaconda3/bin/jupyter lab --ip 0.0.0.0 --notebook-dir $HOME '--KernelSpecManager.whitelist=["pytorch_env"]'
 
 [Install]
 WantedBy=multi-user.target
@@ -50,19 +46,5 @@ EOL
 sudo mv /tmp/jupyter.service /lib/systemd/system/jupyter.service
 sudo systemctl start jupyter.service
 sudo systemctl enable jupyter.service
-
-## Add the update fastai script
-cat > ~/update-fastai.sh <<EOL
-#!/bin/bash
-
-source activate fastai-v1
-conda update -y -c pytorch pytorch-nightly cuda92
-conda update -y -c fastai torchvision-nightly
-conda update -y -c fastai fastai
-
-sudo systemctl restart jupyter
-EOL
-
-chmod +x ~/update-fastai.sh
 
 sudo reboot
